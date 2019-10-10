d41dd54d3913b41bfceea2d06e8c4310070bce3f
alongvili <poshil@outlook.co.il>
2019-10-10 14:41:28 +0300

    first init

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# UDAntd Demo Site
+
+## Build the site
+
+-   clone the universal dashboard repo
+-   build the ud module
+-   copy the output folder to the site root folder
+-   clone the universaldashboard.antd repo
+-   build the universaldashboard.antd module
+-   copy universaldashboard.antd output folder the the site root folder
---
+++ output/dashboard.ps1
@@ -0,0 +1,78 @@
+Import-Module $PSScriptRoot\UniversalDashboard.Antd\UniversalDashboard.Antd.psd1 -Force
+
+$Theme = Get-UDTheme -Name Default
+$Theme.Definition['main'] = @{padding = 'unset'}
+
+$Dashboard = New-UDDashboard -Title UDAntd -Content {
+    New-UDAntdDrawer -Title Antd -Placement right -Content { } -Closable -Width 600 -MaskClosable -Id 'mainDrawer'
+    New-UDAntdLayout -Content {
+
+        # site header
+        New-UDAntdHeader -Content {
+
+            New-UDAntdMenu -Content {
+
+                New-UDAntdMenuItem -Title Components -Content {
+        
+                    New-UDAntdIcon -Icon LayoutOutline -Size 2x 
+        
+                } -Style @{display = 'flex'; alignItems = 'center' }
+
+                New-UDAntdMenuItem -Title Test -Content {
+        
+                    New-UDAntdIcon -Icon GithubOutline -Size 2x 
+        
+                } -Style @{display = 'flex'; alignItems = 'center' }
+
+                New-UDAntdMenuItem -Title Test1 -Content {
+        
+                    New-UDAntdIcon -Icon GitlabOutline -Size 2x 
+        
+                } -Style @{display = 'flex'; alignItems = 'center' }
+            
+            } -Style @{height = 64; display = 'flex'}
+        
+        } -Style @{marginBottom = 50; position = 'fixed'; padding = 0; width = '100%'; zIndex = 1 }
+
+        New-UDAntdContent -Content {
+
+            New-UDAntdButton -Label "OPEN DRAWER" -OnClick {
+                Set-UDElement -Id 'mainDrawer' -Attributes @{visible = $true}
+            }
+
+            New-UDAntdButton -Label "Change TimeLine Mode" -OnClick {
+                Set-UDElement -Id 'timeline' -Attributes @{mode = 'left'}
+            }
+
+            New-UDAntdButton -Label "Change TimeLine Mode To Alternate" -OnClick {
+                Set-UDElement -Id 'timeline' -Attributes @{mode = 'alternate' }
+            }
+
+            New-UDAntdButton -Label "ADD TO TIMELINE" -OnClick {
+                Add-UDElement -ParentId 'timeline' -Content {
+                    New-UDAntdTimeLineItem -Content {
+                        "New item were crated"
+                    } -Color 'green'
+
+                }
+            }
+    
+            New-UDAntdTimeLine -Id 'timeline' -Content {
+                New-UDAntdTimeLineItem -Content {
+                    "Computers were crated"
+                } -Color 'pink'
+                New-UDAntdTimeLineItem -Content {
+                    "PowerShell was created"
+                } -Color 'blue'
+                New-UDAntdTimeLineItem -Content {
+                    "Universal Dashboard was created"
+                } -Color 'red'
+            } -Mode alternate
+        } -Style @{padding = 50; marginTop = 128; height = '100vh'}
+    }
+    
+} -Theme $Theme
+
+$Dashboard.FrameworkAssetId = [UniversalDashboard.Services.AssetService]::Instance.Frameworks["Antd"]
+
+Start-UDDashboard -Wait -Dashboard $Dashboard -Force
