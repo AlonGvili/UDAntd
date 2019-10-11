6c320dfe17f6d912f4de25ef4a8877b155c295d5
alongvili <poshil@outlook.co.il>
2019-10-11 03:44:13 +0300

    add syntex highlight to the webapp

--- output/dashboard.ps1
+++ output/dashboard.ps1
@@ -1,5 +1,5 @@
-Import-Module $PSScriptRoot\UniversalDashboard.Antd\UniversalDashboard.Antd.psd1 -Force
-
+Import-Module "$PSScriptRoot\UniversalDashboard.Antd\UniversalDashboard.Antd.psd1" -Force
+Import-Module "$PSScriptRoot\UniversalDashboard.SyntaxHighlighter\1.0.0\UniversalDashboard.SyntaxHighlighter.psd1" -Force
 # import webapp styles variables
 Import-Module -Variable * $PSScriptRoot\webappStyles.ps1
 
@@ -12,7 +12,24 @@ $Dashboard = New-UDDashboard -Title UDAntd -Content {
     # web app reused components
     New-UDAntdDrawer -Id 'reused_drawer_right' -Title Antd -Placement right -Content { } -Closable -Width 600 -MaskClosable 
     New-UDAntdPopover -Id 'reused_popover_top' -Title { 'AntdPopover' } -Placement top -Content { } -Children { } 
-    New-UDAntdCard -Id 'codeBox' -Style $code_style -Bordered -Content { } -Title 'command name example' -HeadStyle $code_header_style
+    $CodeBox = New-UDAntdCard -Id 'codeBox' -Style $code_style -Bordered -Content {
+        New-UDSyntaxHighlighter -Language PowerShell -Style github -Code "New-UDAntdMenu -Id 'mainNavbar' -Style $navbar_style -Content {
+
+            New-UDAntdMenuItem -Style $navbar_item_style -Title Components -Content {
+                New-UDAntdIcon -Icon AppstoreFill -Size 2x 
+            } 
+
+            New-UDAntdMenuItem -Style $navbar_item_style -Title Test -Content {
+                New-UDAntdIcon -Icon GithubFill -Size 2x
+            } 
+
+            New-UDAntdMenuItem -Style $navbar_item_style -Title Test1 -Content {
+                New-UDAntdIcon -Icon CodeFill -Size 2x
+            } 
+        
+        } 
+"
+     } -Title 'command name example' -HeadStyle $code_header_style
 
     # web app main layout
     New-UDAntdLayout -Id 'mainLayout' -Style $layout_style -Content {
@@ -43,7 +60,9 @@ $Dashboard = New-UDDashboard -Title UDAntd -Content {
         # web app content
         New-UDAntdContent -Id 'mainContent' -Style $content_style -Content {
 
-            New-UDAntdButton -Label Drawer -OnClick { Set-UDElement -Id 'reused_drawer_right' -Attributes @{visible = $true} }
+            New-UDAntdButton -Label Drawer -OnClick { Set-UDElement -Id 'reused_drawer_right' -Attributes @{ visible = $true } }
+
+            $CodeBox
         } 
 
     }
