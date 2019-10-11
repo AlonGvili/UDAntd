6c241ef7a3e81c796053855e57b1c0f2d3c4e937
alongvili <poshil@outlook.co.il>
2019-10-11 04:37:49 +0300

    ...

--- output/dashboard.ps1
+++ output/dashboard.ps1
@@ -12,24 +12,9 @@ $Dashboard = New-UDDashboard -Title UDAntd -Content {
     # web app reused components
     New-UDAntdDrawer -Id 'reused_drawer_right' -Title Antd -Placement right -Content { } -Closable -Width 600 -MaskClosable 
     New-UDAntdPopover -Id 'reused_popover_top' -Title { 'AntdPopover' } -Placement top -Content { } -Children { } 
-    $CodeBox = New-UDAntdCard -Id 'codeBox' -Style $code_style -Bordered -Content {
-        New-UDSyntaxHighlighter -Language PowerShell -Style github -Code "New-UDAntdMenu -Id 'mainNavbar' -Style $navbar_style -Content {
-
-            New-UDAntdMenuItem -Style $navbar_item_style -Title Components -Content {
-                New-UDAntdIcon -Icon AppstoreFill -Size 2x 
-            } 
-
-            New-UDAntdMenuItem -Style $navbar_item_style -Title Test -Content {
-                New-UDAntdIcon -Icon GithubFill -Size 2x
-            } 
-
-            New-UDAntdMenuItem -Style $navbar_item_style -Title Test1 -Content {
-                New-UDAntdIcon -Icon CodeFill -Size 2x
-            } 
-        
-        } 
-"
-     } -Title 'command name example' -HeadStyle $code_header_style
+    $CodeBox = New-UDAntdCard -Id 'codeBox' -Style $code_style -Title 'command name example' -HeadStyle $code_header_style -Bordered -Content {
+        New-UDSyntaxHighlighter -Language PowerShell -Style vs -Code ""
+     } 
 
     # web app main layout
     New-UDAntdLayout -Id 'mainLayout' -Style $layout_style -Content {
@@ -60,9 +45,26 @@ $Dashboard = New-UDDashboard -Title UDAntd -Content {
         # web app content
         New-UDAntdContent -Id 'mainContent' -Style $content_style -Content {
 
-            New-UDAntdButton -Label Drawer -OnClick { Set-UDElement -Id 'reused_drawer_right' -Attributes @{ visible = $true } }
+            New-UDAntdMenu -Id 'componentsMenu' -Mode inline -Style @{width = 250} -Content {
+                New-UDAntdSubMenu -Title { New-UDAntdIcon -Icon AppstoreOutline -Size 2x; "Basic Components" } -Content {
+                    New-UDAntdMenuItemGroup -Title Basic -Content {
+                        New-UDAntdMenuItem -Title 'Card' -Content {"Card"}
+                        New-UDAntdMenuItem -Title 'Button' -Content {"Button"}
+                        New-UDAntdMenuItem -Title 'Button Group' -Content {"Button Group"}
+                        New-UDAntdMenuItem -Title 'Radio' -Content {"Radio"}
+                        New-UDAntdMenuItem -Title 'Radio Group' -Content {"Radio Group"}
+                        New-UDAntdMenuItem -Title 'Switch' -Content {"Switch"}
+                    } 
+                }
+                New-UDAntdSubMenu -Title { New-UDAntdIcon -Icon ApiOutline -Size 2x; "Input Components" } -Content {
+                    New-UDAntdMenuItemGroup -Title Input -Content {
+                        New-UDAntdMenuItem -Title 'Text Box' -Content {"Text Box"}
+                        New-UDAntdMenuItem -Title 'Text Area' -Content {"Text Area"}
+                        New-UDAntdMenuItem -Title 'Password Box' -Content {"Password Box"}
+                    } 
+                }
+            }
 
-            $CodeBox
         } 
 
     }
