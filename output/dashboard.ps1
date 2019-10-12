1ec0f293da51f3f7444213e18bedf153ebcd2c31
alongvili <poshil@outlook.co.il>
2019-10-12 08:38:50 +0300

    ...

--- output/dashboard.ps1
+++ output/dashboard.ps1
@@ -18,7 +18,7 @@ function Update-ComponentContentSection {
     Set-Item -Path "Cache:CommandDoc" -Value $Doc
     Set-Item -Path "Cache:CommandExample" -Value $Example
     Set-UDElement -Id 'componentInfoContent' -Content { 
-        $MarkdownDoc = Get-Content -Path "$PSScriptRoot\UniversalDashboard.Antd\Docs\$Doc" -Raw
+        $MarkdownDoc = Get-Content -Path "/Docs/$Doc" -Raw
         New-UDMarkdown -Markdown $MarkdownDoc 
     }
 }
