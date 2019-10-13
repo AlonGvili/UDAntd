8c1bf58b6bb224468f734e050a4a57fdd337f8ee
alongvili <poshil@outlook.co.il>
2019-10-14 00:30:10 +0300

    ....

--- output/install.modules.ps1
+++ output/install.modules.ps1
@@ -1,20 +1,21 @@
-$Pass = ConvertTo-SecureString -String "$($ENV:APPSETTING_GITHUB_TOKEN)" -AsPlainText -Force -ErrorAction Stop
+$Pass = ConvertTo-SecureString -String $ENV:APPSETTING_GITHUB_TOKEN -AsPlainText -Force -ErrorAction Stop
 $Cred = [System.Management.Automation.PSCredential]::new('AlonGvili', $Pass)
 
-if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
-    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
-}
+# if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
+#     Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
+# }
 
-if ($Null -eq (Get-InstalledModule -Name PowerShellGet -ErrorAction SilentlyContinue)) {
-    Install-Module PowerShellGet -AllowClobber -Scope CurrentUser -Force;
-    Remove-Module -Name PowerShellGet -ErrorAction SilentlyContinue;
-    Import-Module -Name PowerShellGet -Force;
-}
+# if ($Null -eq (Get-InstalledModule -Name PowerShellGet -ErrorAction SilentlyContinue)) {
+#     Install-Module PowerShellGet -AllowClobber -Scope CurrentUser -Force;
+#     Remove-Module -Name PowerShellGet -ErrorAction SilentlyContinue;
+#     Import-Module -Name PowerShellGet -Force;
+# }
 
 if ($Null -eq (Get-InstalledModule -Name PowerShellForGitHub -ErrorAction SilentlyContinue)) {
     Install-Module PowerShellForGitHub -Scope CurrentUser -Force;
     Import-Module PowerShellForGitHub -Force
     Set-GitHubAuthentication -Credential $Cred
+    Set-GitHubConfiguration -DisableTelemetry 
 }
 
 if ($Null -eq (Get-InstalledModule -Name UniversalDashboard.Helmet -ErrorAction SilentlyContinue)) {
@@ -37,9 +38,11 @@ if ($Null -eq (Get-InstalledModule -Name PSDocs -ErrorAction SilentlyContinue)) 
     Import-Module PSDocs -Force
 }
 
+Write-Verbose -Message "Start download the module from github" -Verbose
 Invoke-WebRequest (Get-GitHubRelease -OwnerName AlonGvili -RepositoryName UniversalDashboard.Antd -Latest | 
     Select-Object -expand assets | 
     Select-Object  -expand browser_download_url ) -OutFile $PSScriptRoot\UniversalDashboard.Antd.zip
 Expand-Archive -Path $PSScriptRoot\UniversalDashboard.Antd.zip -DestinationPath $PSScriptRoot\UniversalDashboard.Antd -Force
+Write-Verbose -Message "Finish download the module from github" -Verbose
 
 Copy-Item -Path $PSScriptRoot\Docs\* -Destination $PSScriptRoot\UniversalDashboard.Antd\Docs -Recurse -Force
