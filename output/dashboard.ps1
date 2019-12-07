6bd66e379e5694b82ceee04ef17b12d9acbee7ca
alongvili <poshil@outlook.co.il>
2019-12-07 03:02:33 +0200

    ssd

--- output/dashboard.ps1
+++ output/dashboard.ps1
@@ -18,8 +18,8 @@ $Root = $PSScriptRoot
 
 $Dashboard = New-UDDashboard -Title "Universal Dashboard Ant-Design" -Content {
 
-    $LivePreviewExamplesDB = Get-Content -Path D:\local\VirtualDirectory0\site\wwwroot\data\d.json -Raw | ConvertFrom-Json
-    # $LivePreviewExamplesDB = Invoke-RestMethod  https://udantd.site/data/d.json
+    # $Global:LivePreviewExamplesDB = Get-Content -Path D:\local\VirtualDirectory0\site\wwwroot\data\d.json -Raw | ConvertFrom-Json
+    $LivePreviewExamplesDB = Invoke-RestMethod  https://raw.githubusercontent.com/AlonGvili/UDAntd/master/output/data/d.json
     # web app main layout
     New-UDAntdLayout -Id 'mainLayout' -Style $WebAppStyles['Webapp'] -Content {
 
@@ -141,7 +141,7 @@ $Dashboard = New-UDDashboard -Title "Universal Dashboard Ant-Design" -Content {
 
 $Dashboard.FrameworkAssetId = [UniversalDashboard.Services.AssetService]::Instance.Frameworks["Antd"]
 
-$Folder = Publish-UDFolder -Path D:\local\VirtualDirectory0\site\wwwroot\data\ -RequestPath "/data"
-Start-UDDashboard -Wait -Dashboard $Dashboard -Force -PublishedFolder $Folder 
+# $Folder = Publish-UDFolder -Path D:\local\VirtualDirectory0\site\wwwroot\data\ -RequestPath "/data"
+Start-UDDashboard -Wait -Dashboard $Dashboard -Force 
 # Start-UDDashboard -Dashboard $Dashboard -Force -PublishedFolder $Folder -Port 1004
 
